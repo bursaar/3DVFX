5d55261e6d3f4441a938d4661c281251cb10d782
bursaar <ben@ben.ie>
2014-03-06 17:38:18 +0000

    Created 2D Triangle

--- Main.cpp
+++ Main.cpp
@@ -8,13 +8,15 @@
 #pragma comment (lib, "d3d9.lib")
 
 // global declarations
-LPDIRECT3D9 d3d;				// the pointer to our Direct3D interface
-LPDIRECT3DDEVICE9 d3ddev;		// the pointer to the device class
+LPDIRECT3D9 d3d;							// the pointer to our Direct3D interface
+LPDIRECT3DDEVICE9 d3ddev;					// the pointer to the device class
+LPDIRECT3DVERTEXBUFFER9 v_buffer = NULL;    // the pointer to the vertex buffer
 
 // function prototypes
 void initD3D(HWND hWnd);    // sets up and initializes Direct3D
 void render_frame(void);    // renders a single frame
 void cleanD3D(void);		// closes Direct3D and releases memory
+void init_graphics(void);    // 3D declarations
 
 // the WindowProc function prototype
 LRESULT CALLBACK WindowProc(HWND hWnd,
@@ -23,6 +25,12 @@ LRESULT CALLBACK WindowProc(HWND hWnd,
 	LPARAM lParam);
 
 DWORD backgroundColour = D3DCOLOR_XRGB(backgroundRed, backgroundGreen, backgroundBlue);
+
+struct CUSTOMVERTEX
+{
+	FLOAT x, y, z, rhw;    // from the D3DFVF_XYZRHW flag
+	DWORD color;    // from the D3DFVF_DIFFUSE flag
+};
 
 // the entry point for any Windows program
 int WINAPI WinMain(HINSTANCE hInstance,
@@ -149,6 +157,8 @@ void initD3D(HWND hWnd)
 		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
 		&d3dpp,
 		&d3ddev);
+
+	init_graphics();    // call the function to initialize the triangle
 }
 
 // this is the function used to render a single frame
@@ -160,6 +170,14 @@ void render_frame(void)
 	d3ddev->BeginScene();    // begins the 3D scene
 
 	// do 3D rendering on the back buffer here
+	// select which vertex format we are using
+	d3ddev->SetFVF(CUSTOMFVF);
+
+	// select the vertex buffer to display
+	d3ddev->SetStreamSource(0, v_buffer, 0, sizeof(CUSTOMVERTEX));
+
+	// copy the vertex buffer to the back buffer
+	d3ddev->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
 
 	d3ddev->EndScene();    // ends the 3D scene
 
@@ -170,6 +188,34 @@ void render_frame(void)
 // this is the function that cleans up Direct3D and COM
 void cleanD3D(void)
 {
+	v_buffer->Release();    // close and release the vertex buffer
 	d3ddev->Release();    // close and release the 3D device
 	d3d->Release();    // close and release Direct3D
+}
+
+// this is the function that puts the 3D models into video RAM
+void init_graphics(void)
+{
+	// create the vertices using the CUSTOMVERTEX struct
+	CUSTOMVERTEX vertices[] =
+	{
+		{ 400.0f, 62.5f, 0.5f, 1.0f, D3DCOLOR_XRGB(0, 0, 255), },
+		{ 650.0f, 500.0f, 0.5f, 1.0f, D3DCOLOR_XRGB(0, 255, 0), },
+		{ 150.0f, 500.0f, 0.5f, 1.0f, D3DCOLOR_XRGB(255, 0, 0), },
+	};
+
+	// create a vertex buffer interface called v_buffer
+	d3ddev->CreateVertexBuffer(3 * sizeof(CUSTOMVERTEX),
+		0,
+		CUSTOMFVF,
+		D3DPOOL_MANAGED,
+		&v_buffer,
+		NULL);
+
+	VOID* pVoid;    // a void pointer
+
+	// lock v_buffer and load the vertices into it
+	v_buffer->Lock(0, 0, (void**)&pVoid, 0);
+	memcpy(pVoid, vertices, sizeof(vertices));
+	v_buffer->Unlock();
 }
--- Screen Properties.h
+++ Screen Properties.h
@@ -1,9 +1,12 @@
 // define the screen resolution
 #define SCREEN_WIDTH  1280
 #define SCREEN_HEIGHT 800
+#define CUSTOMFVF (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
 
 // Backround Colours
 int backgroundAlpha = 255;
 int backgroundRed = 90;
 int backgroundGreen = 40;
 int backgroundBlue = 100;
+
+int changingColour = 255;
